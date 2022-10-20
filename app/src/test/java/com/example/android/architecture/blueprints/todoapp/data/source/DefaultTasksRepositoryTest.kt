3367b88f51e7b1834e9419fe6afb6d258ef1b1db
brian.businge <brian.businge@engie.com>
2022-10-20 13:03:20 +0300

    edited the tasks

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/data/source/DefaultTasksRepositoryTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/data/source/DefaultTasksRepositoryTest.kt
@@ -13,9 +13,9 @@ import org.junit.Test
 
 @ExperimentalCoroutinesApi
 class DefaultTasksRepositoryTest {
-    private val task1 = Task("Title1", "Description1")
-    private val task2 = Task("Title2", "Description2")
-    private val task3 = Task("Title3", "Description3")
+    private val task1 = Task("Head to Mapeera", "meet the loan officer")
+    private val task2 = Task("Visit security premises", "Assert that the security is real")
+    private val task3 = Task("Dispatch money", "Transfer money to account")
     private val remoteTasks = listOf(task1, task2).sortedBy { it.id }
     private val localTasks = listOf(task3).sortedBy { it.id }
     private val newTasks = listOf(task3).sortedBy { it.id }
